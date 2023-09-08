6736b5e591b5a02bb54765b2d124d0e9cd77bbb0
Manulapo <you@example.com>
2023-09-08 19:11:21 +0200

    set Reducers for user and Cart

--- src/components/contexts/user.context.jsx
+++ src/components/contexts/user.context.jsx
@@ -1,9 +1,10 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useReducer, useEffect } from "react";
 
 import {
   createUserDocumentFromAuth,
   onAuthStateChangedListener,
 } from "../../utils/firebase/firebase.utils";
+import { createAction } from "../../utils/reducer/reducer.utils";
 
 // init with passing the default values to createConetxt method
 export const UserContext = createContext({
@@ -11,8 +12,39 @@ export const UserContext = createContext({
   setCurrentUser: () => null, //if no context
 });
 
+export const USER_ACTION_TYPES = {
+  SET_CURRENT_USER: "SET_CURRENT_USER",
+};
+
+const INITIAL_STATE = {
+  currentUser: null,
+};
+
+// reducer way start
+const userReducer = (state, action) => {
+  const { type, payload } = action;
+
+  switch (type) {
+    case USER_ACTION_TYPES.SET_CURRENT_USER:
+      return {
+        ...state, //spread trhough the current state +  current user new value
+        currentUser: payload,
+      };
+
+    default:
+      throw new Error(`Unhandles type ${type} in userReducer`);
+  }
+};
+
 export const UserProvider = ({ children }) => {
-  const [currentUser, setCurrentUser] = useState(null);
+  // userReducer utilized
+  const [{ currentUser }, dispatch] = useReducer(userReducer, INITIAL_STATE);
+
+  const setCurrentUser = (user) => {
+    // to get an action i have to use dispatch function
+    dispatch(createAction(USER_ACTION_TYPES.SET_CURRENT_USER, user));
+  };
+
   const value = { currentUser, setCurrentUser };
 
   useEffect(() => {
